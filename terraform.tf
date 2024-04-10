0799ac482b5afc994744157732210e46b91fa8d1
Paul Strebenitzer <paulstrebenitzer@MBP-von-Paul.fritz.box>
2024-04-10 13:58:52 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "TFC_Tutorial_PS"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
